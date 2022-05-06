aabeebd994e85372c1e9ba07f70ccb1bc1b58511
MdShohan81 <mdshohan6563@gmail.com>
2022-05-06 11:03:11 +0600

    changes _id

--- src/components/ItemCard/ItemCard.js
+++ src/components/ItemCard/ItemCard.js
@@ -7,7 +7,7 @@ const ItemCard = (props) => {
     const {_id,name,picture,price,quantity,ServiceName, reviewText} = props.item;
     const navigate = useNavigate();
     const navigateToUpdate = id =>{
-        navigate(`product/${_id}`)
+        navigate(`product/${id}`)
     }
     return (
         <div>
