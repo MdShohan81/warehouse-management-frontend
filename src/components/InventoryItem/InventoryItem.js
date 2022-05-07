8f463612d5548877c76aa8eb6a3b72257912f7d8
MdShohan81 <mdshohan6563@gmail.com>
2022-05-07 18:55:01 +0600

    added delivered btn

--- src/components/InventoryItem/InventoryItem.js
+++ src/components/InventoryItem/InventoryItem.js
@@ -5,7 +5,6 @@ import { toast, ToastContainer } from 'react-toastify';
 
 const InventoryItem = () => {
     const { inventoryId } = useParams();
- 
     const [item, setItem] = useState({});
     useEffect(() => {
         const url =  `http://localhost:5000/product/${inventoryId}`;
@@ -14,66 +13,53 @@ const InventoryItem = () => {
         .then(data => setItem(data))
     }, []);
 
-    const handleUpdateItem = event => {
-        event.preventDefault();
-        const email = event.target.email.value;
-        const url = event.target.url.value;
-        const ServiceName = event.target.ServiceName.value;
-        const price = event.target.price.value;
-        const quantity = event.target.quantity.value;
-
-        const updatedItem = {email, url,ServiceName,price,quantity}
-
-        const urls = `http://localhost:5000/product/${inventoryId}`;
-        fetch(urls, {
-            method: 'PUT',
-            headers: {
-                'content-type': 'application/json'
-            },
-            body: JSON.stringify(updatedItem)
-        })
-        .then(res => res.json())
-        .then(result => {
-            console.log(result);
-            toast('item updated successfully');
-            event.target.reset();
-            
-        })
-    }
+   const handleReduceQuantity = () =>{
+       const quantity = parseInt(item.quantity) - 1;
+       const updateQuantity = { quantity };
+       const url = `http://localhost:5000/product/${inventoryId}`;
+       fetch(url, {
+           method: 'PUT',
+           headers: {
+               'content-type': 'application/json'
+           },
+           body: JSON.stringify(updateQuantity)
+       })
+       .then(res => res.json())
+       .then(data => {
+           console.log('success', data);
+           alert('Delivery success');
+           
+       });
+   }
     
     return (
         <div className='container'>
             <div className="row">
                 <h2>Items Name: {item.ServiceName}</h2>
-                <div className="col-md-6">
-                <Form onSubmit={handleUpdateItem}>
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Control type="email" name="email" placeholder="Enter email"/>
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="formBasicName">
-                        <Form.Control type="url" name="url" placeholder="Photo url" />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="formBasicName">
-                        <Form.Control type="text" name="ServiceName" placeholder="Item Name" required/>
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="formBasicName">
-                        <Form.Control type="text" name="name" placeholder="Supplier Name" required/>
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="formBasicName">
-                        <Form.Control type="number" name="price" placeholder="price" required/>
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="formBasicName">
-                        <Form.Control type="number" name="quantity" placeholder="quantity" required/>
-                    </Form.Group>
-                    <Button variant="primary" type="submit">
-                        Delivered 
-                    </Button>
-                </Form>
+                <div className="col-md-8">
+                <div className="card mb-3">
+                <div className="row g-0">
+                    <div className="col-md-5">
+                    <img src={item.picture} className="img-fluid w-100" alt="img"/>
+                    </div>
+                    <div className="col-md-7">
+                    <div className="card-body">
+                        <small className="card-title">id: <span className='text-danger'> {item._id}</span></small>
+                        <h5 className="card-title">{item.ServiceName}</h5>
+                        <h3 className="card-title text-danger">${item.price}</h3>
+                        <h6 className="text-danger">supplier: {item.name}</h6>
+                        <h6 className="text-danger">quantity: {item.quantity}</h6>
+                        <span className='card-title'>Description:</span>
+                        <p className="card-text"><small>{item.reviewText}</small></p>
+                        <button onClick={handleReduceQuantity} className='btn btn-danger'>Delivered</button>
+                    </div>
+                    </div>
+                </div>
+                </div>
                 <ToastContainer></ToastContainer>
                 </div>
-                <div className="col-md-6">
+                <div className="col-md-4">
                 <Form>
-                    
                     <Form.Group className="mb-3" controlId="formBasicName">
                         <Form.Control type="number" name="quantity" placeholder="quantity" required/>
                     </Form.Group>
